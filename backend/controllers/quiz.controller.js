fec10f3121e1766f979c7c4ff6ce750011efd979
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-12-20 14:02:38 +0530

    update/delete

--- backend/controllers/quiz.controller.js
+++ backend/controllers/quiz.controller.js
@@ -30,8 +30,32 @@ const getQuizByID = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+const updateQuiz = async (req, res) => {
+    try {
+        const quiz = await Quiz.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (quiz) {
+            res.json(quiz);
+        } else {
+            res.status(404).json({ message: 'Quiz not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
+const deleteQuiz = async (req, res) => {
+    try {
+        const quiz = await Quiz.findByIdAndDelete(req.params.id);
+        if (quiz) {
+            res.json({ message: 'Quiz deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Quiz not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = {
-    CreateQuiz, getQuiz, getQuizByID
+    CreateQuiz, getQuiz, getQuizByID, updateQuiz ,deleteQuiz
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,13 +4,15 @@ const userRouter = express.Router();
 const { login, signup} = require("../controllers/user.controller");
 
 const { authorization } = require('../middlewares/authorization');
-const { CreateQuiz, getQuiz, getQuizByID }=require("../controllers/quiz.controller")
+const { CreateQuiz, getQuiz, getQuizByID, updateQuiz, deleteQuiz }=require("../controllers/quiz.controller")
 
 userRouter.post("/login", login);
 userRouter.post("/signup", signup);
 userRouter.get("/quizzes", authorization, getQuiz);
 userRouter.get("/quizzes/:id", authorization, getQuizByID);
 userRouter.post("/quizzes", authorization, CreateQuiz);
+userRouter.put('/quizzes/:id', authorization, updateQuiz,);
+userRouter.delete('/quizzes/:id', authorization,deleteQuiz);
 
 
 
